a45245bc696fb3ac5a5830969168a02066038d79
kyr8er <labajokerr5@gmail.com>
2023-06-03 00:44:22 +0800

    Modify RecursiveBSTReeDepthCalculator program

--- Assignment/RecursiveBSTreeDepthCalculator/Program.cs
+++ Assignment/RecursiveBSTreeDepthCalculator/Program.cs
@@ -1,35 +1,28 @@
-﻿namespace RecursiveBSTreeDepthCalculator
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Enter root value of tree: ");
-            int root = int.Parse(Console.ReadLine()!);
-            var tree = new BSTree(root);
-            while(true)
-            {
-                Console.Write("Enter a number to add to the tree (or 'q' to quit): ");
-                var input = Console.ReadLine();
-                if (input == "q")
-                {
-                    break;
-                }
-                else
-                {
-                    if (int.TryParse(input, out var node))
-                    {
-                        tree.Add(node);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input.");
-                    }
-                }
-            }
+﻿using RecursiveBSTreeDepthCalculator;
 
-            Console.WriteLine($"Depth: {tree.GetDepth()}");
-            tree.PrintInOrder();
+Console.WriteLine("Enter root value of tree: ");
+int root = int.Parse(Console.ReadLine()!);
+var tree = new BSTree(root);
+while (true)
+{
+    Console.Write("Enter a number to add to the tree (or 'q' to quit): ");
+    var input = Console.ReadLine();
+    if (input == "q")
+    {
+        break;
+    }
+    else
+    {
+        if (int.TryParse(input, out var node))
+        {
+            tree.Add(node);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
         }
     }
 }
+
+Console.WriteLine($"Depth: {tree.GetDepth()}");
+tree.PrintInOrder();
