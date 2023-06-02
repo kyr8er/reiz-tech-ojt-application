031627633b40a75e6838135784638bd9ec07c82a
kyr8er <labajokerr5@gmail.com>
2023-06-02 23:33:52 +0800

    Fix program in RecursiveBSTreeDepthCalculator

--- Assignment/RecursiveBSTreeDepthCalculator/Program.cs
+++ Assignment/RecursiveBSTreeDepthCalculator/Program.cs
@@ -1,21 +1,35 @@
-﻿using RecursiveBSTreeDepthCalculator;
-
-var root = new Branch();
-
-while(true)
+﻿namespace RecursiveBSTreeDepthCalculator
 {
-    Console.WriteLine("Enter a number to add to the tree or 'q' to quit");
-    string input = Console.ReadLine()!;
-    if(input == "q")
+    internal class Program
     {
-        break;
-    }
-    else
-    {
-        int node = int.Parse(input);
-        root.Add(node);
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter root value of tree: ");
+            int root = int.Parse(Console.ReadLine()!);
+            var tree = new BSTree(root);
+            while(true)
+            {
+                Console.Write("Enter a number to add to the tree (or 'q' to quit): ");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    if (int.TryParse(input, out var node))
+                    {
+                        tree.Add(node);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input.");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Depth: {tree.GetDepth()}");
+            tree.PrintTree();
+        }
     }
 }
-
-Console.WriteLine($"Depth of tree is {root.Depth()}");
-root.PrintTree();
