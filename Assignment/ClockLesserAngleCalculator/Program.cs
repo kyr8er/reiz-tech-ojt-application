1c27d314a1ace537d571954581e1a8458799ce28
kyr8er <labajokerr5@gmail.com>
2023-06-02 20:36:38 +0800

    Implement program

--- Assignment/ClockLesserAngleCalculator/Program.cs
+++ Assignment/ClockLesserAngleCalculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int hour;
+int minute;  
+
+try
+{
+    Console.WriteLine("Enter hour: ");
+    hour = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter minute: ");
+    minute = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Angle between {hour} and {minute} is {ClockLesserAngleCalculator.Calculate(hour, minute)} degrees");
+
+}catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
