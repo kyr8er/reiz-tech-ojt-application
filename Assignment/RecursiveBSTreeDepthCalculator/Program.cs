bdb39dc98e1807d22abaee8a3201e6a66435ea21
kyr8er <labajokerr5@gmail.com>
2023-06-02 22:37:44 +0800

    Implement Recursive BSTreeDepthCalculator program

--- Assignment/RecursiveBSTreeDepthCalculator/Program.cs
+++ Assignment/RecursiveBSTreeDepthCalculator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RecursiveBSTreeDepthCalculator;
+
+var root = new Branch();
+
+while(true)
+{
+    Console.WriteLine("Enter a number to add to the tree or 'q' to quit");
+    string input = Console.ReadLine()!;
+    if(input == "q")
+    {
+        break;
+    }
+    else
+    {
+        int node = int.Parse(input);
+        root.Add(node);
+    }
+}
+
+Console.WriteLine($"Depth of tree is {root.Depth()}");
+root.PrintTree();
