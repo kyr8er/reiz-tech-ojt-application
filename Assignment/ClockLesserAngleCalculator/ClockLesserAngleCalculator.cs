f735988ba472c06cb8d6dc19930cb82010268cdc
kyr8er <labajokerr5@gmail.com>
2023-06-02 20:34:42 +0800

    Add ClockLesserAngleCalculator class

---
+++ Assignment/ClockLesserAngleCalculator/ClockLesserAngleCalculator.cs
@@ -0,0 +1,31 @@
+﻿internal class ClockLesserAngleCalculator
+{
+    public static int Calculate(int hour, int minute)
+    {
+        int angle;
+        int hourAngle;
+        int minuteAngle;
+
+        if (hour < 0 || hour > 12)
+        {
+            throw new ArgumentException("Hour must be between 0 and 12");
+        }
+
+        if (minute < 0 || minute > 60)
+        {
+            throw new ArgumentException("Minute must be between 0 and 60");
+        }
+
+        hourAngle = (hour * 360) / 12;
+        minuteAngle = (minute * 360) / 60;
+
+        angle = Math.Abs(hourAngle - minuteAngle);
+
+        if (angle > 180)
+        {
+            angle = 360 - angle;
+        }
+
+        return angle;
+    }
+}
