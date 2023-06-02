66bc36d85eb81262d01d89c572a1ef0cb60b4074
kyr8er <labajokerr5@gmail.com>
2023-06-02 23:33:06 +0800

    Add Left and Right branch

--- Assignment/RecursiveBSTreeDepthCalculator/Branch.cs
+++ Assignment/RecursiveBSTreeDepthCalculator/Branch.cs
@@ -3,72 +3,70 @@
     internal class Branch
     {
         public List<Branch> Branches { get; set; }
+
         public int Node { get; set; }
+
+        public Branch? Left
+        {
+            get
+            {
+                if (Branches.Count > 0)
+                {
+                    return Branches[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                if (Branches.Count > 0)
+                {
+                    Branches[0] = value!;
+                }
+                else
+                {
+                    Branches.Add(value!);
+                }
+            }
+        }
+
+        public Branch? Right
+        {
+            get
+            {
+                if (Branches.Count > 1)
+                {
+                    return Branches[1];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                if (Branches.Count > 1)
+                {
+                    Branches[1] = value!;
+                }
+                else
+                {
+                    Branches.Add(value!);
+                }
+            }
+        }
 
         public Branch()
         {
             Branches = new List<Branch>();
         }
 
-        private Branch(int node)
+        public Branch(int node)
         {
             Node = node;
             Branches = new List<Branch>();
-        }
-
-        public void Add(int Node)
-        {
-            if(Node < this.Node)
-            {
-                if(Branches.Any())
-                {
-                    Branches[0].Add(Node);
-                }
-                else
-                {
-                    Branches.Add(new Branch(Node));
-                }
-            }
-            else
-            {
-                if(Branches.Count > 1)
-                {
-                    Branches[1].Add(Node);
-                }
-                else
-                {
-                    Branches.Add(new Branch(Node));
-                }
-            }
-        }
-
-        public int Depth()
-        {
-            if(!Branches.Any())
-            {
-                return 1;
-            }
-            else
-            {
-                return 1 + Branches.Max(b => b.Depth());
-            }
-        }
-
-        public void PrintTree(string prefix = "")
-        {
-            Console.WriteLine($"{prefix}{Node}");
-
-            for (int i = 0; i < Branches.Count; i++)
-            {
-                if (i == 0)
-                {
-                    Branches[i].PrintTree(prefix + "|-- ");
-                }
-                else
-                {
-                    Branches[i].PrintTree(prefix + "└-- ");
-                }
-            }
         }
     }
 }
